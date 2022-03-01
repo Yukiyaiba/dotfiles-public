60f7ffe3c9586b98c12a522db117f38dd6387a3d
Yukiyaiba <94126756+Yukiyaiba@users.noreply.github.com>
2022-03-01 15:10:14 +0100

    Update OS_FineTuning_Manjaro_Min.sh

--- OS_FineTuning_Manjaro_Min.sh
+++ OS_FineTuning_Manjaro_Min.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
 
+sudo pacman -R onlyoffice-desktopeditors qbittorrent thunderbird thunderbird-i18n-hu thunderbird-i18n-en-us yakuake gwenview
+
+sudo pacman-mirrors -g
+
+sudo pacman -Syyu
+
+sudo pacman -S  wine
+
+sudo pacman -R  wine
+
+sudo pacman -S  lutris
+
+sudo pacman -S  clamtk
+
+sudo pacman -S  mc nano nomacs btop libreoffice-still libreoffice-still-hu gimp inkscape krita qreator glabels chromium deluge-gtk opera vivaldi filezilla atril engrampa krename rawtherapee pdfarranger
